29e89351020d3a07e02e3e33492eff27250b9ab8
Richard Dao <rdao2003@gmail.com>
2024-06-08 13:00:10 -0700

    Imported heatmap

--- apps/birds/static/js/map.js
+++ apps/birds/static/js/map.js
@@ -5,62 +5,18 @@ function toRaw(obj) {
     return obj.__v_raw || obj;
 }
 
-function calculateConvexHull(points) {
-    // Clone the points array to avoid modifying the original array
-    const clonedPoints = points.slice();
-    
-    // Sort points by x-coordinate (in case of tie, sort by y-coordinate)
-    clonedPoints.sort((a, b) => {
-        if (a.lat === b.lat) {
-            return a.lng - b.lng;
-        }
-        return a.lat - b.lat;
-    });
-
-    // Define helper functions for cross product and cross product sign
-    function crossProduct(p1, p2, p3) {
-        return (p2.lat - p1.lat) * (p3.lng - p1.lng) - (p3.lat - p1.lat) * (p2.lng - p1.lng);
-    }
-
-    function crossProductSign(p1, p2, p3) {
-        const value = crossProduct(p1, p2, p3);
-        return value === 0 ? 0 : (value > 0 ? 1 : -1);
-    }
-
-    // Initialize lower and upper hull arrays
-    const lowerHull = [];
-    const upperHull = [];
-
-    // Build lower hull
-    for (const point of clonedPoints) {
-        while (lowerHull.length >= 2 && crossProductSign(lowerHull[lowerHull.length - 2], lowerHull[lowerHull.length - 1], point) <= 0) {
-            lowerHull.pop();
-        }
-        lowerHull.push(point);
-    }
-
-    // Build upper hull
-    for (let i = clonedPoints.length - 1; i >= 0; i--) {
-        const point = clonedPoints[i];
-        while (upperHull.length >= 2 && crossProductSign(upperHull[upperHull.length - 2], upperHull[upperHull.length - 1], point) <= 0) {
-            upperHull.pop();
-        }
-        upperHull.push(point);
-    }
-
-    // Remove duplicates and combine lower and upper hulls
-    upperHull.pop();
-    lowerHull.pop();
-    return lowerHull.concat(upperHull);
-}
-
 let app = {};
 
 app.data = {    
     data: function() {
         return {
             map: null,
+            map_bounds: [],
             user_location: null,
+
+            events_in_bounds: [],
+
+
             drawing_coords: [],
             points: [],
             polygons: []
@@ -82,6 +38,7 @@ app.data = {
                 .bindPopup("Current Location")
                 .openPopup();
             this.map.setView(this.user_location, 13);
+            this.map.once('moveend', this.loadHeatMap);
         },
 
         showLocationError: function(error) {
@@ -140,6 +97,18 @@ app.data = {
             this.points = [];
             this.drawing_coords = [];
 
+        },
+
+        loadHeatMap: function() {
+            var heat = L.heatLayer([
+                [this.user_location[0], this.user_location[1], 1],
+            ], {radius: 25, maxOpacity: 1}).addTo(toRaw(this.map));
+            // First, get all events that are inside of the box
+            
+            // Then, loop through all events and record the sightings for each event
+            // Each sighting will be of format species + count
+            // Come up with a formula to denote intensity for a bird
+            
         }
 
     },
@@ -156,13 +125,23 @@ app.data = {
         }).addTo(map);
         map.on('click', this.mapClickListener);
         map.on('dblclick', this.mapDblClickListener);
+        map.on('moveend', function() {
+            this.map_bounds = map.getBounds();
+            console.log(" North: " + map.getBounds().getNorth() + " South: " + map.getBounds().getSouth() + " East: " + map.getBounds().getEast() + " West: " + map.getBounds().getWest())
+            
+        });
+
         map.doubleClickZoom.disable();
+
+        map.whenReady(() => {
+            this.getUserLocation();
+        });
+
         setTimeout(() => {
             map.invalidateSize();
         }, 100);
 
         this.map = map;
-        this.getUserLocation();
 
     }
 };
