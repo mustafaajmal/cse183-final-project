b4a5044670bd755757c41f11d69f961c6ad6056d
Nathan <natedolor@gmail.com>
2024-06-07 18:19:50 -0700

    adding search bar and statistics functionality

--- apps/birds/controllers.py
+++ apps/birds/controllers.py
@@ -41,7 +41,8 @@ def index():
     return dict(
         # COMPLETE: return here any signed URLs you need.
         my_callback_url = URL('my_callback', signer=url_signer),
-        get_user_statistics_url = URL('get_user_statistics')
+        get_user_statistics_url = URL('get_user_statistics'),
+        search_url = URL('search'),
     )
 
 @action('checklist')
@@ -62,7 +63,9 @@ def location():
 @action.uses('user_statistics.html', db, auth.user, url_signer)
 def user_statistics():
     return dict(
-        load_user_statistics_url = URL('load_user_statistics')
+        load_user_statistics_url = URL('load_user_statistics'),
+        search_url = URL('search'),
+        observation_dates_url = URL('observation_dates')
     )
     
 @action('load_user_statistics')
@@ -71,8 +74,43 @@ def get_user_statistics():
     query = (db.sightings.OBSERVATION_COUNT.regexp('^[0-9]+$')) & (db.sightings.OBSERVATION_COUNT.cast('integer') > 0)
 
     common_names = db(query).select(db.sightings.COMMON_NAME, distinct=True).as_list()
-    print("common names", common_names)
+    print("user statistics contains all birds")
     return dict(common_names = common_names)
+
+@action('search', method=["POST"])
+@action('search')
+@action.uses(db, auth.user, url_signer)
+def search():
+    data = request.json  # Get the JSON payload
+    q = data.get("params", {}).get("q")  # Extract 'q' from 'params'
+    print("query", q)
+    if not q:
+        common_names = db(query).select(db.sightings.COMMON_NAME, distinct=True).as_list()
+        print("user statistics contains all birds")
+        return dict(common_names = common_names)
+    
+    query = (db.sightings.OBSERVATION_COUNT.regexp('^[0-9]+$')) & (db.sightings.OBSERVATION_COUNT.cast('integer') > 0)
+    query &= (db.sightings.COMMON_NAME.contains(q))
+    
+    common_names = db(query).select(db.sightings.COMMON_NAME, distinct=True).as_list()
+    print("searched by query" , q)
+    return dict(common_names=common_names)
+    
+@action('observation_dates', method=["POST"]) 
+@action('observation_dates')
+@action.uses(db, auth.user, url_signer)
+def observation_date():
+    data = request.json
+    common_name = data.get("common_name")
+    print("retrieving observation dates by ", common_name)
+    if not common_name:
+        print("ERROR: common_name not found")
+        return dict(observation_dates=[])
+    
+    query = (db.sightings.COMMON_NAME == common_name) & (db.sightings.SAMPLING_EVENT_IDENTIFIER == db.sightings.SAMPLING_EVENT_IDENTIFIER)
+    observation_dates = db(query).select(db.checklist.OBSERVATION_DATE).as_list()
+    print("observation_dates: ", observation_dates)
+    return dict(observation_dates=observation_dates)
 
 @action('my_callback')
 @action.uses() # Add here things like db, auth, etc.
@@ -98,5 +136,6 @@ def my_callback():
                 checklist = db.sightings.insert(name=row[0])
 
     return dict(my_value=3, species = species, sightings = sightings, checklist = checklist)
+
     '''
 
